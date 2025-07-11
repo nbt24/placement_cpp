8cd17b019a2cd63c4f7805def0d370409c79f0e9
Aditya Jain <jainaditya412@gmail.com>
2025-07-11 09:06:38 +0530

    Create 1-2.cpp
    final code

---
+++ 1-2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+/*
+1 - write a funtion main
+2 - print hello dosto on the screen
+*/
+int main()
+{
+    // print something on the screen
+    cout << "\t\t\t\t\t\tHello dosto!" << endl;
+    cout << "                    Welcome to AJ Sir Class!" << "\n";
+    cout << 24 << endl;
+    cout << 2+4 << endl;
+    cout << 4-2 << endl;
+    cout << 3*4 << endl;
+    
+    return 0;
+}
+
+Data types:
+
+integer --> int --> eg: -23, -1, -4, 2, 5, 67, 102
+decimal --> float/double --> 2.4, 78.3, -9.7
+character --> char --> 'a', 'A', '5', '@', '#', ' ' (ASCII codes)
+string --> string --> "Aditya Jain", "Hello"
+boolean --> bool --> true/false
+
+
+syntax:
+datatype variable_name = value;
+
+string name = "AJ";
+
+
+Variable naming:
+
+Rule1-> start: a-z,A-Z,_
+Rule2-> between: a-z, A-Z, _, 0-9
+
+name
+firstname
+1- firstName, lastName
+2- first_name, last_name
+
+int age = 18;
+string bloodGroup = "B+";
+double years_of_experience = 7.5;
+bool graduate = true;
+
+
+
+
+
+// a
+// d
+// b
+// c
