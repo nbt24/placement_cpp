b21c7d1d45cc6144ef6dde0cf84c4b2dc4dc3f92
Aditya Jain <jainaditya412@gmail.com>
2025-07-14 09:11:00 +0530

    Create 1-3.cpp
    final

---
+++ 1-3.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // char grade = 'A';
+    // bool pass = false;
+    
+    // // true --> 1, false --> 0
+    // cout << grade << endl;
+    // cout << pass << endl;
+    
+    int grade = 97;
+    cout << grade;
+    
+}
+
+/*
+
+int main()
+{
+    int n = 5;
+    cout << n << endl;
+    cout << "n" << endl;
+    n = 10;
+    cout << n << endl;
+    return 1;
+}
+
+
+
+
+// Create a variable name
+// Store some name in it
+// Print "Hello name" onto the screen, where name is the stored name.
+
+int main()
+{
+    string name = "AJ sir";
+    cout << "Hello name" << endl;
+    cout << "Hello " << name << endl;
+    
+    cin >> name;
+    cout << "Hello " << name << endl;
+    
+    int a = 4, b = 6;
+    
+    // int sum = a+b;
+    cout << a+b << endl;
+    
+    const int a = 5;
+    a = 10;
+    cout << a << endl;
+    
+    const int a;
+    a = 10;
+    cout << a << endl;
+    
+    
+}
+
+
+
+// Take two values as input from the user:
+// 1. length 
+// 2. breadth
+
+// Print the area of the rectangle.
+
+int main()
+{
+    int length, breadth, area;
+    cout << "Enter Length: ";
+    cin >> length;
+    
+    cout << "Enter Breadth: ";
+    cin >> breadth;
+    
+    area = length*breadth;
+    
+    cout << area << endl;
+}
+
+*/
